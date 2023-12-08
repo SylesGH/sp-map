606e59520feae536644031d3b0bab886b77ffa6c
ahmetoren51@gmail.com <ahmetoren51@gmail.com>
2023-12-08 07:17:22 +0300

    last touches

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ if (localStorage.getItem("tutorial played")) {
     document.querySelector(".tutorial-card").style.display = "none";
 }
 
+var loadingDelay = 2000
+
+var currentPlayerPos = {
+    x: 140,
+    y: 90,
+}
+
+var currentDestination = {
+    x: 692,
+    y: 493,
+}
+
+setTimeout(() => {
+    informationToggle(document.querySelector(".information"))
+}, loadingDelay + 1000);
+
 function openMenu(el) {
     el.classList.toggle("active");
 }
@@ -33,7 +49,7 @@ function calcRange(obj1, obj2, radius) {
     return distance <= radius
 }
 
-function placeDestination(el) {
+function placeDestination(el, saveto) {
     userResolution = [window.innerWidth, window.innerHeight]
     var pinProperties = {
         coordX: Math.floor(Math.random() * (userResolution[0] - 100)) + 50,
@@ -44,9 +60,14 @@ function placeDestination(el) {
     range[1].y = pinProperties.coordY
     
     el.style = `left: ${pinProperties.coordX}px; top: ${pinProperties.coordY}px;`
+
+    saveto.x = pinProperties.coordX
+    saveto.y = pinProperties.coordY
 }
 
-placeDestination(destination)
+placeDestination(destination, currentPlayerPos)
+placeDestination(destination, currentDestination)
+resetGame()
 
 slots.forEach((slot, index) => {
     new MutationObserver(() => {
@@ -138,344 +159,6 @@ function setState(el) {
     }
 }
 
-/* TODO: NEED A FIX 
-
-var coordinates = [
-    {
-        "x": 83,
-        "y": 94
-    },
-    {
-        "x": 131,
-        "y": 92
-    },
-    {
-        "x": 147,
-        "y": 165
-    },
-    {
-        "x": 255,
-        "y": 169
-    },
-    {
-        "x": 266,
-        "y": 172
-    },
-    {
-        "x": 328,
-        "y": 196
-    },
-    {
-        "x": 363,
-        "y": 197
-    },
-    {
-        "x": 419,
-        "y": 197
-    },
-    {
-        "x": 470,
-        "y": 153
-    },
-    {
-        "x": 519,
-        "y": 125
-    },
-    {
-        "x": 530,
-        "y": 162
-    },
-    {
-        "x": 618,
-        "y": 143
-    },
-    {
-        "x": 703,
-        "y": 131
-    },
-    {
-        "x": 261,
-        "y": 253
-    },
-    {
-        "x": 279,
-        "y": 209
-    },
-    {
-        "x": 167,
-        "y": 300
-    },
-    {
-        "x": 63,
-        "y": 243
-    },
-    {
-        "x": 126,
-        "y": 398
-    },
-    {
-        "x": 106,
-        "y": 416
-    },
-    {
-        "x": 153,
-        "y": 476
-    },
-    {
-        "x": 189,
-        "y": 542
-    },
-    {
-        "x": 249,
-        "y": 490
-    },
-    {
-        "x": 342,
-        "y": 500
-    },
-    {
-        "x": 279,
-        "y": 402
-    },
-    {
-        "x": 324,
-        "y": 330
-    },
-    {
-        "x": 367,
-        "y": 398
-    },
-    {
-        "x": 363,
-        "y": 496
-    },
-    {
-        "x": 374,
-        "y": 583
-    },
-    {
-        "x": 572,
-        "y": 559
-    },
-    {
-        "x": 516,
-        "y": 490
-    },
-    {
-        "x": 547,
-        "y": 285
-    },
-    {
-        "x": 368,
-        "y": 297
-    },
-    {
-        "x": 596,
-        "y": 297
-    },
-    {
-        "x": 647,
-        "y": 366
-    },
-    {
-        "x": 633,
-        "y": 441
-    },
-    {
-        "x": 688,
-        "y": 481
-    },
-    {
-        "x": 688,
-        "y": 419
-    },
-    {
-        "x": 685,
-        "y": 593
-    },
-    {
-        "x": 739,
-        "y": 578
-    },
-    {
-        "x": 808,
-        "y": 655
-    },
-    {
-        "x": 811,
-        "y": 569
-    },
-    {
-        "x": 882,
-        "y": 601
-    },
-    {
-        "x": 902,
-        "y": 566
-    },
-    {
-        "x": 864,
-        "y": 516
-    },
-    {
-        "x": 748,
-        "y": 405
-    },
-    {
-        "x": 695,
-        "y": 299
-    },
-    {
-        "x": 706,
-        "y": 224
-    },
-    {
-        "x": 773,
-        "y": 217
-    },
-    {
-        "x": 605,
-        "y": 222
-    },
-    {
-        "x": 455,
-        "y": 205
-    },
-    {
-        "x": 828,
-        "y": 251
-    },
-    {
-        "x": 926,
-        "y": 178
-    },
-    {
-        "x": 863,
-        "y": 301
-    },
-    {
-        "x": 1015,
-        "y": 552
-    },
-    {
-        "x": 971,
-        "y": 494
-    },
-    {
-        "x": 970,
-        "y": 438
-    },
-    {
-        "x": 964,
-        "y": 184
-    },
-    {
-        "x": 1045,
-        "y": 223
-    },
-    {
-        "x": 1061,
-        "y": 173
-    },
-    {
-        "x": 1106,
-        "y": 242
-    },
-    {
-        "x": 1123,
-        "y": 165
-    },
-    {
-        "x": 1218,
-        "y": 139
-    },
-    {
-        "x": 1283,
-        "y": 142
-    },
-    {
-        "x": 1317,
-        "y": 190
-    },
-    {
-        "x": 1398,
-        "y": 246
-    },
-    {
-        "x": 1324,
-        "y": 273
-    },
-    {
-        "x": 1362,
-        "y": 393
-    },
-    {
-        "x": 1399,
-        "y": 480
-    },
-    {
-        "x": 1298,
-        "y": 494
-    },
-    {
-        "x": 1265,
-        "y": 411
-    },
-    {
-        "x": 1253,
-        "y": 458
-    },
-    {
-        "x": 1215,
-        "y": 383
-    },
-    {
-        "x": 1135,
-        "y": 374
-    },
-    {
-        "x": 1190,
-        "y": 469
-    },
-    {
-        "x": 1164,
-        "y": 526
-    },
-    {
-        "x": 1119,
-        "y": 469
-    },
-    {
-        "x": 1064,
-        "y": 354
-    },
-    {
-        "x": 1055,
-        "y": 293
-    },
-    {
-        "x": 1037,
-        "y": 400
-    },
-    {
-        "x": 805,
-        "y": 153
-    },
-    {
-        "x": 1184,
-        "y": 266
-    },
-    {
-        "x": 330,
-        "y": 259
-    }
-]
-
-coordinates.forEach(cor => {
-    var span = `<span class="pin" style="left: ${cor.x * resolutionRatio.X}px; top: ${(cor.y * resolutionRatio.Y) + ((window.innerHeight - document.querySelector(".container img").clientHeight) / 2 )}px;"></span>`
-    document.querySelector(".container").innerHTML += span
-}) */
-
 // debug
 function debugMode() {
     var cityCoordinates = [];
@@ -515,12 +198,34 @@ function debugMode() {
     document.addEventListener("mousemove", (e) => {
         indicator.style = `left: ${e.clientX + 5}px; top: ${e.clientY - (indicator.clientHeight - 1)}px;`
         indicator.querySelector("#coordinates").innerText = `X ${e.clientX} Y ${e.clientY}`
-    })   
+    })  
+    
+    document.addEventListener("click", (e) => {
+        range[0].x = e.clientX
+        range[0].y = e.clientY
+    
+        if (e.target == document.querySelector("img")) {
+            document.querySelector(".player").style = `
+            top: ${e.clientY}px;
+            left: ${e.clientX}px;
+            ` 
+            if (calcRange(range[0], range[1], 50)) {
+                document.querySelector(".popup").classList.remove("hidden")
+            } else {
+                document.querySelector(".popup").classList.add("hidden")
+            }
+        }
+    });
+}
+
+function resetGame() {
+    document.querySelector(".player").style = `top: ${currentPlayerPos.y}px; left: ${currentPlayerPos.x}px`
+    destination.style = `top: ${currentDestination.y}px; left: ${currentDestination.x}px`
 }
 
 function restartGame() {
     setTimeout(() => {
-        placeDestination(destination)
+        placeDestination(destination, currentDestination)
         document.querySelector(".popup").classList.add("hidden")
     }, 100);
 }
@@ -539,23 +244,6 @@ function hideTutorial() {
     }, 300);
     localStorage.setItem("tutorial played", true)
 }
-
-document.addEventListener("click", (e) => {
-    range[0].x = e.clientX
-    range[0].y = e.clientY
-
-    if (e.target == document.querySelector("img")) {
-        document.querySelector(".player").style = `
-            top: ${e.clientY}px;
-            left: ${e.clientX}px;
-        ` 
-        if (calcRange(range[0], range[1], 20)) {
-            document.querySelector(".popup").classList.remove("hidden")
-        } else {
-            document.querySelector(".popup").classList.add("hidden")
-        }
-    }
-});
 
 function updateDataBlocks() {
   dataBlocks = [];
@@ -576,7 +264,19 @@ function clearSlots() {
     })
 }
 
-function playGame() {
+function playGame(el) {
+    const checkSlots = [];
+
+    slots.forEach(slot => {
+        checkSlots.push(slot.getAttribute("data-block"));
+    });
+
+    if (el.classList.contains("disabled") || checkSlots.every(item => item === "")) {
+        return
+    }
+
+    el.classList.add("disabled")
+
     var player = document.querySelector(".player")
 
     var playerCoord = {
@@ -586,49 +286,83 @@ function playGame() {
 
     var playerFrames = []
 
-    var steps = 50
+    var steps = Math.floor(((window.innerHeight / 10) + (window.innerWidth / 10)) / 1.8)
 
-    var moveset = JSON.parse(localStorage.getItem("block moveset"))
-    for (let i = 0; i < moveset.length; i++) {
-        if (moveset[i]) {
-            switch (moveset[i]) {
-                case "up":
-                    playerCoord.y -= steps
-                    playerFrames.push({direction: "y", amount: playerCoord.y})
-                    break;
+    try {
+        var moveset = JSON.parse(localStorage.getItem("block moveset"))
 
-                case "right":
-                    playerCoord.x += steps
-                    playerFrames.push({direction: "x", amount: playerCoord.x})
-                    break;
-
-                case "down":
-                    playerCoord.y += steps
-                    playerFrames.push({direction: "y", amount: playerCoord.y})
-                    break;
-
-                case "left":
-                    playerCoord.x -= steps
-                    playerFrames.push({direction: "x", amount: playerCoord.x})
-                    break;
+        for (let i = 0; i < moveset.length; i++) {
+            if (moveset[i]) {
+                switch (moveset[i]) {
+                    case "up":
+                        playerCoord.y -= steps
+                        playerFrames.push({direction: "y", amount: playerCoord.y})
+                        break;
+    
+                    case "right":
+                        playerCoord.x += steps
+                        playerFrames.push({direction: "x", amount: playerCoord.x})
+                        break;
+    
+                    case "down":
+                        playerCoord.y += steps
+                        playerFrames.push({direction: "y", amount: playerCoord.y})
+                        break;
+    
+                    case "left":
+                        playerCoord.x -= steps
+                        playerFrames.push({direction: "x", amount: playerCoord.x})
+                        break;
+                }
             }
         }
+    } catch (e) {
+        el.classList.remove("disabled")
+        return
     }
 
-    var lastX = 0
-    var lastY = 0
+    var lastX = parseInt(player.style.left) || 0
+    var lastY = parseInt(player.style.top) || 0
+    var lastDelay = 0
+    var gameState
 
     for (let i = 0; i < playerFrames.length; i++) {
-        console.log(playerFrames[i])
-        if (playerFrames[i].direction == "x") {
-            lastX = playerFrames[i].amount
-        } else {
-            lastY = playerFrames[i].amount
-        }
-
+        lastDelay = i * 1000 + 1000
         setTimeout(() => {
+            if (playerFrames[i].direction == "x") {
+                lastX = playerFrames[i].amount
+            } else {
+                lastY = playerFrames[i].amount
+            }
+
             player.style = `top: ${playerFrames[i].direction == "y" ? playerFrames[i].amount : lastY}px; left: ${playerFrames[i].direction == "x" ? playerFrames[i].amount : lastX}px`;
+
+            gameState = calcRange(playerCoord, currentDestination, steps)
+            
+            if (gameState) {
+                setTimeout(() => {
+                    document.querySelector(".popup").classList.remove("hidden")
+                }, lastDelay + 500);
+            }
         }, i * 1000);
     }
-    
+
+    setTimeout(() => {
+        el.classList.remove("disabled")
+        setTimeout(() => {
+            if (!gameState) {
+                popupToggle(document.querySelector(".retry"))
+            }
+        }, 1000);
+    }, lastDelay);     
+}
+
+function informationToggle(el) {
+    setTimeout(() => {
+        el.classList.toggle("minimized")
+    }, 100);
+}
+
+function popupToggle(el) {
+    el.classList.toggle("hidden")
 }
